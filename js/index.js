87d5fbeed83dc86e4a36cd6a08a532d7c73e9f04
netnr <netnr@netnr.com>
2019-08-20 17:11:49 +0800

    update

--- js/index.js
+++ js/index.js
@@ -2,29 +2,32 @@
     keywords = keywords.trim().toLowerCase();
     var fb = $('#favoritebox');
     fb.find('a').each(function () {
+        var col = $(this).parent().parent().parent();
         if (keywords != "") {
-            if (this.innerHTML.toLowerCase().indexOf(keywords) >= 0 || this.title.toLowerCase().indexOf(keywords) >= 0 || this.href.toLowerCase().indexOf(keywords) >= 0) {
-                $(this).parent().removeClass('d-none');
+            if ($(this).text().toLowerCase().indexOf(keywords) >= 0 || this.href.toLowerCase().indexOf(keywords) >= 0) {
+                col.removeClass('d-none');
             } else {
-                $(this).parent().addClass('d-none');
+                col.addClass('d-none');
             }
         } else {
-            $(this).parent().removeClass('d-none');
+            col.removeClass('d-none');
         }
     });
     fb.children().each(function () {
+        var cols = $(this).children();
         if (group != "") {
-            if ($(this).attr('data-group') == group) {
+            if (cols.first().text().trim() == group) {
                 $(this).removeClass('d-none');
             } else {
                 $(this).addClass('d-none');
             }
         } else {
             $(this).removeClass('d-none');
-        }
-        var ga = $(this).find('div.mba');
-        if (ga.length == ga.filter('.d-none').length) {
-            $(this).addClass('d-none');
+            if (cols.filter('.d-none').length + 1 >= cols.length) {
+                cols.first().addClass('d-none');
+            } else {
+                cols.first().removeClass('d-none');
+            }
         }
     });
 }
@@ -39,28 +42,6 @@ $('#btnSearch').click(function () {
 $('#txtSearch').on('input', function () {
     search($('#seGroup').val(), localStorage["favorite-search"] = $('#txtSearch').val())
 });
-
-setTimeout(function () {
-    $('#favoritebox').find('a').each(function () {
-        var hs = this.href.split('/');
-        hs.length = 3;
-        var img = $(this).find('img');
-        img[0].onerror = function () { this.src = '/images/net.svg'; this.onerror = null; }
-        var imgsrc = '/images/net.svg';
-        if (img.attr('data-icon') != "") {
-            imgsrc = img.attr('data-icon');
-        } else {
-            if (this.href.indexOf('http://') == -1) {
-                imgsrc = hs.join('/') + "/favicon.ico";
-            }
-        }
-        var ci = new Image();
-        ci.src = imgsrc;
-        ci.onload = function () {
-            img[0].src = imgsrc;
-        }
-    });
-}, 1000);
 
 $(document).dblclick(function () {
     $('#txtSearch').val('');
@@ -78,3 +59,11 @@ $('#seGroup').find('option').each(function () {
     }
 });
 $('#txtSearch')[0].focus();
+
+$('#favoritebox').find('img').each(function () {
+    var that = this;
+    this.onerror = function () { this.src = '/favicon.svg'; this.onerror = null; }
+    var ci = new Image();
+    ci.onload = function () { that.src = this.src; }
+    ci.src = that.getAttribute('data-icon');
+});
--- src/Netnr.ScriptServices/Netnr.ScriptServices/wwwroot/js/global.js
+++ src/Netnr.ScriptServices/Netnr.ScriptServices/wwwroot/js/global.js
@@ -1,123 +1,113 @@
-﻿//netnrnav
-(function ($) {
-    $.fn.netnrnav = function (options) {
-        options = options || {};
-        var that = this,
-            isHiding = false,
-            _event = 0, // 这个临时变量是处理多级树事件判断的，很重要
-            defaultOptions = {
-                closingCascade: true // 级联关闭所有菜单，仅对垂直导航菜单有效
-            };
-        if (!$(that).hasClass('netnrnav')) {
-            if ($(this).find('.netnrnav').length < 1)
-                return;
-            that = $(this).find('.netnrnav')[0];
-        }
-        defaultOptions = $.extend({}, defaultOptions, options);
-        var _init = function () {
-            if ($(that).hasClass('horizontal')) {
-                $(that).find('li').hover(function () {
+﻿/*
+ * netnrnav v1.1.0
+ * 
+ * https://github.com/netnr/nav
+ * 
+ * Fork: https://github.com/zhoufengjob/SuiNav
+ * 
+ * Date: 2019-08-18
+ * 
+ */
+
+(function (window) {
+
+    var netnrnav = function (se) { return new netnrnav.fn.init(se); };
+
+    netnrnav.fn = netnrnav.prototype = {
+        init: function (se) {
+            var that = this;
+            this.ele = $(se);
+            this.eventCount = 0;
+            this.isHiding = false;
+
+            if (that.ele.hasClass('horizontal')) {
+                that.ele.find('li').hover(function () {
                     $(this).children('ul').show();
                 }, function () {
                     $(this).children('ul').hide();
                 });
             } else {
-                $(that).find('li').click(function () {
-                    if (_event != 0) {
-                        // 这里很重要，处理事件回调
+                that.ele.find('li').click(function () {
+                    if (that.eventCount != 0) {
                         if ($(this).parent().parent().parent().hasClass('netnrnav')) {
-                            _event = 0;
+                            that.eventCount = 0;
                         }
                         return;
                     }
-                    if ("none" == $(this).children('ul').css('display'))
+                    if ($(this).children('ul').is(":hidden"))
                         $(this).children('ul').show();
                     else {
-                        if (defaultOptions.closingCascade) {
-                            $(this).find('ul').hide();
-                        } else {
-                            $(this).children('ul').hide();
-                        }
+                        $(this).find('ul').hide();
                     }
-                    _event++;
+                    that.eventCount++;
                     if ($(this).parent().parent().parent().hasClass('netnrnav')) {
-                        _event = 0;
+                        that.eventCount = 0;
                     }
                 });
             }
-            $(window).resize(_resize);
-        },
-            _show = function () {
-                if (isHiding)
-                    return;
-                $(document.body).append('<div class="netnrnav slide-nav"></div>');
-                $(document.body).append('<div class="netnrnav nav-mask"></div>');
-                $('.slide-nav').html($(that).html());
-                $('.slide-nav').find('li').click(function () {
-                    if (_event != 0) {
-                        // 这里很重要，处理事件回调
-                        if ($(this).parent().parent().parent().hasClass('netnrnav')) {
-                            _event = 0;
-                        }
-                        return;
-                    }
-                    if ("none" == $(this).children('ul').css('display'))
-                        $(this).children('ul').show();
-                    else {
-                        if (defaultOptions.closingCascade) {
-                            $(this).find('ul').hide();
-                        } else {
-                            $(this).children('ul').hide();
-                        }
-                    }
-                    _event++;
-                    if ($(this).parent().parent().parent().hasClass('netnrnav')) {
-                        _event = 0;
-                    }
-                });
-                $('.nav-mask').click(function () {
-                    _hide();
-                });
-                // 某些浏览器有毒，加个小小的延迟让动画完整展示
-                setTimeout(function () {
-                    $('.slide-nav').toggleClass('active');
-                    $('.nav-mask').toggleClass('active');
-                }, 20);
-            },
-            _hide = function () {
-                if (isHiding)
-                    return;
-                isHiding = true;
-                $('.slide-nav').find('li').unbind();
-                $('.slide-nav').removeClass('active');
-                $('.nav-mask').removeClass('active');
-                setTimeout(function () {
-                    $('.slide-nav').remove();
-                    $('.nav-mask').remove();
-                    isHiding = false;
-                }, 600);
-            },
-            _toggle = function () {
-                ($('.slide-nav').length > 0) ? _hide() : _show();
-            },
-            _resize = function () {
-                // 窗口改变时需要完成的事情，预留着吧
-            };
-        _resize();
-        _init();
-        return {
-            show: _show,
-            hide: _hide,
-            toggle: _toggle
-        };
+            return this;
+        }
     };
-})($);
+
+    netnrnav.show = function (that) {
+        if (!that.isHiding) {
+            $(document.body).append('<div class="netnrnav slide-netnrnav"></div><div class="netnrnav netnrnav-mask"></div>');
+            $('.slide-netnrnav').html(that.ele.html()).find('li').click(function () {
+                if (that.eventCount != 0) {
+                    if ($(this).parent().parent().parent().hasClass('netnrnav')) {
+                        that.eventCount = 0;
+                    }
+                    return;
+                }
+                if ($(this).children('ul').is(":hidden"))
+                    $(this).children('ul').show();
+                else {
+                    $(this).find('ul').hide();
+                }
+                that.eventCount++;
+                if ($(this).parent().parent().parent().hasClass('netnrnav')) {
+                    that.eventCount = 0;
+                }
+            });
+            $('.netnrnav-mask').click(function () {
+                netnrnav.hide(that);
+            });
+            setTimeout(function () {
+                $('.slide-netnrnav').toggleClass('active');
+                $('.netnrnav-mask').toggleClass('active');
+            }, 20);
+        }
+    };
+
+    netnrnav.hide = function (that) {
+        if (!that.isHiding) {
+            that.isHiding = true;
+            $('.slide-netnrnav').find('li').unbind();
+            $('.slide-netnrnav').removeClass('active');
+            $('.netnrnav-mask').removeClass('active');
+            setTimeout(function () {
+                $('.slide-netnrnav').remove();
+                $('.netnrnav-mask').remove();
+                that.isHiding = false;
+            }, 600);
+        }
+    };
+
+    netnrnav.toggle = function (that) {
+        $('.slide-netnrnav').length > 0 ? netnrnav.hide(that) : netnrnav.show(that)
+    }
+
+    netnrnav.fn.init.prototype = netnrnav.fn;
+
+    window.netnrnav = netnrnav;
+
+})(window);
 
 $(function () {
-    var nnv = $('#netnrnav');
-    if (nnv.length) {
-        $('.MenuToggle').click(nnv.netnrnav().toggle);
-    }
+    $.nrnav = netnrnav(".netnrnav");
+    $('.MenuToggle').click(function () {
+        netnrnav.toggle($.nrnav);
+    });
 });
 
 var ss = {
